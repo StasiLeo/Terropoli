82d8b732d8400a74fe82867515715440446a5103
StasiLeo <leonardo.stasi@barsanti.edu.it>
2024-09-20 15:16:56 +0200

    nothing

---
+++ Assets/movauto.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movauto : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Machinna;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
